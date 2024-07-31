546ec572a02028db415b20243eb1bfe1f7f4ed41
Jordon Douglas <53793958+jordondoug2019@users.noreply.github.com>
2024-07-31 13:29:20 -0500

    Update application.py

--- application.py
+++ application.py
@@ -666,4 +666,4 @@ if __name__ == '__main__':
     application.secret_key = 'super_secret_key'
     application.debug = True
     port = int(os.environ.get('PORT', 5000))
-    application.run(host='0.0.0.0', port=8080)
+    application.run(host='0.0.0.0', port=port)
